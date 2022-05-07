92bb3545d243c1f1379e1b528a8196ad2c4ac52f
Dr. Derek Austin <derekraustin@gmail.com>
2022-05-07 16:53:15 -0500

    chore: extend spacing in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        15: "3.75rem",
+        96: "24rem",
+        104: "26rem",
+        112: "28rem",
+        114: "28.5rem",
+        116: "29rem",
+        120: "30rem",
+        128: "32rem",
+        144: "36rem",
+        160: "42rem",
+      },
+    },
   },
   plugins: [],
 }
