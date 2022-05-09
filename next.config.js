fb9ff996a193de705663d38c5bf34de226a1f87e
Dr. Derek Austin <derekraustin@gmail.com>
2022-05-09 11:42:37 -0500

    feat: add "img.pokemondb.net" as allowed domain for next/image in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // Configure next/image to use the website returned by the GraphQL API:
+    domains: ["img.pokemondb.net"],
+  },
 }
 
 module.exports = nextConfig
