501a2d2e9ebc7cf312d0334cc28719df12a4eb30
Dr. Derek Austin <derekraustin@gmail.com>
2022-05-08 22:39:40 -0500

    feat: wrap _app.tsx in react-query helpers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import "@/styles/globals.css"
 
+import { QueryClient, QueryClientProvider } from "react-query"
+
 import GlobalStateContext from "@/components/GlobalStateContext"
 import authMachine from "@/utils/authMachine"
 import { useInterpret } from "@xstate/react"
 
 import type { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }: AppProps) {
+const queryClient = new QueryClient()
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   const authService = useInterpret(authMachine)
+
   return (
-    <GlobalStateContext.Provider value={{ authService }}>
-      {/* We can access this context throughout the app because of .Provider */}
-      <Component {...pageProps} />
-    </GlobalStateContext.Provider>
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <GlobalStateContext.Provider value={{ authService }}>
+        {/* We can access React context throughout the app via Provider */}
+        <Component {...pageProps} />
+      </GlobalStateContext.Provider>
+    </QueryClientProvider>
   )
 }
-
-export default MyApp
