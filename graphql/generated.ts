85dc140e9f1dcd2bd88242c6bffdb97a4d9e98ca
Dr. Derek Austin <derekraustin@gmail.com>
2022-05-09 22:19:33 -0500

    chore: format autogenerated types

--- graphql/generated.ts
+++ graphql/generated.ts
@@ -1,137 +1,170 @@
-import { useQuery, UseQueryOptions } from 'react-query';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+import { useQuery, UseQueryOptions } from "react-query"
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>
+}
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>
+}
 
-function fetcher<TData, TVariables>(endpoint: string, requestInit: RequestInit, query: string, variables?: TVariables) {
+function fetcher<TData, TVariables>(
+  endpoint: string,
+  requestInit: RequestInit,
+  query: string,
+  variables?: TVariables
+) {
   return async (): Promise<TData> => {
     const res = await fetch(endpoint, {
-      method: 'POST',
+      method: "POST",
       ...requestInit,
       body: JSON.stringify({ query, variables }),
-    });
+    })
 
-    const json = await res.json();
+    const json = await res.json()
 
     if (json.errors) {
-      const { message } = json.errors[0];
+      const { message } = json.errors[0]
 
-      throw new Error(message);
+      throw new Error(message)
     }
 
-    return json.data;
+    return json.data
   }
 }
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-};
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+}
 
 /** Represents a Pokémon's attack types */
 export type Attack = {
-  __typename?: 'Attack';
+  __typename?: "Attack"
   /** The damage of this Pokémon attack */
-  damage?: Maybe<Scalars['Int']>;
+  damage?: Maybe<Scalars["Int"]>
   /** The name of this Pokémon attack */
-  name?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars["String"]>
   /** The type of this Pokémon attack */
-  type?: Maybe<Scalars['String']>;
-};
+  type?: Maybe<Scalars["String"]>
+}
 
 /** Represents a Pokémon */
 export type Pokemon = {
-  __typename?: 'Pokemon';
+  __typename?: "Pokemon"
   /** The attacks of this Pokémon */
-  attacks?: Maybe<PokemonAttack>;
+  attacks?: Maybe<PokemonAttack>
   /** The classification of this Pokémon */
-  classification?: Maybe<Scalars['String']>;
+  classification?: Maybe<Scalars["String"]>
   /** The evolution requirements of this Pokémon */
-  evolutionRequirements?: Maybe<PokemonEvolutionRequirement>;
+  evolutionRequirements?: Maybe<PokemonEvolutionRequirement>
   /** The evolutions of this Pokémon */
-  evolutions?: Maybe<Array<Maybe<Pokemon>>>;
-  fleeRate?: Maybe<Scalars['Float']>;
+  evolutions?: Maybe<Array<Maybe<Pokemon>>>
+  fleeRate?: Maybe<Scalars["Float"]>
   /** The minimum and maximum weight of this Pokémon */
-  height?: Maybe<PokemonDimension>;
+  height?: Maybe<PokemonDimension>
   /** The ID of an object */
-  id: Scalars['ID'];
-  image?: Maybe<Scalars['String']>;
+  id: Scalars["ID"]
+  image?: Maybe<Scalars["String"]>
   /** The maximum CP of this Pokémon */
-  maxCP?: Maybe<Scalars['Int']>;
+  maxCP?: Maybe<Scalars["Int"]>
   /** The maximum HP of this Pokémon */
-  maxHP?: Maybe<Scalars['Int']>;
+  maxHP?: Maybe<Scalars["Int"]>
   /** The name of this Pokémon */
-  name?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars["String"]>
   /** The identifier of this Pokémon */
-  number?: Maybe<Scalars['String']>;
+  number?: Maybe<Scalars["String"]>
   /** The type(s) of Pokémons that this Pokémon is resistant to */
-  resistant?: Maybe<Array<Maybe<Scalars['String']>>>;
+  resistant?: Maybe<Array<Maybe<Scalars["String"]>>>
   /** The type(s) of this Pokémon */
-  types?: Maybe<Array<Maybe<Scalars['String']>>>;
+  types?: Maybe<Array<Maybe<Scalars["String"]>>>
   /** The type(s) of Pokémons that this Pokémon weak to */
-  weaknesses?: Maybe<Array<Maybe<Scalars['String']>>>;
+  weaknesses?: Maybe<Array<Maybe<Scalars["String"]>>>
   /** The minimum and maximum weight of this Pokémon */
-  weight?: Maybe<PokemonDimension>;
-};
+  weight?: Maybe<PokemonDimension>
+}
 
 /** Represents a Pokémon's attack types */
 export type PokemonAttack = {
-  __typename?: 'PokemonAttack';
+  __typename?: "PokemonAttack"
   /** The fast attacks of this Pokémon */
-  fast?: Maybe<Array<Maybe<Attack>>>;
+  fast?: Maybe<Array<Maybe<Attack>>>
   /** The special attacks of this Pokémon */
-  special?: Maybe<Array<Maybe<Attack>>>;
-};
+  special?: Maybe<Array<Maybe<Attack>>>
+}
 
 /** Represents a Pokémon's dimensions */
 export type PokemonDimension = {
-  __typename?: 'PokemonDimension';
+  __typename?: "PokemonDimension"
   /** The maximum value of this dimension */
-  maximum?: Maybe<Scalars['String']>;
+  maximum?: Maybe<Scalars["String"]>
   /** The minimum value of this dimension */
-  minimum?: Maybe<Scalars['String']>;
-};
+  minimum?: Maybe<Scalars["String"]>
+}
 
 /** Represents a Pokémon's requirement to evolve */
 export type PokemonEvolutionRequirement = {
-  __typename?: 'PokemonEvolutionRequirement';
+  __typename?: "PokemonEvolutionRequirement"
   /** The amount of candy to evolve */
-  amount?: Maybe<Scalars['Int']>;
+  amount?: Maybe<Scalars["Int"]>
   /** The name of the candy to evolve */
-  name?: Maybe<Scalars['String']>;
-};
+  name?: Maybe<Scalars["String"]>
+}
 
 /** Query any Pokémon by number or name */
 export type Query = {
-  __typename?: 'Query';
-  pokemon?: Maybe<Pokemon>;
-  pokemons?: Maybe<Array<Maybe<Pokemon>>>;
-  query?: Maybe<Query>;
-};
-
+  __typename?: "Query"
+  pokemon?: Maybe<Pokemon>
+  pokemons?: Maybe<Array<Maybe<Pokemon>>>
+  query?: Maybe<Query>
+}
 
 /** Query any Pokémon by number or name */
 export type QueryPokemonArgs = {
-  id?: InputMaybe<Scalars['String']>;
-  name?: InputMaybe<Scalars['String']>;
-};
-
+  id?: InputMaybe<Scalars["String"]>
+  name?: InputMaybe<Scalars["String"]>
+}
 
 /** Query any Pokémon by number or name */
 export type QueryPokemonsArgs = {
-  first: Scalars['Int'];
-};
+  first: Scalars["Int"]
+}
 
-export type PokemonsQueryVariables = Exact<{ [key: string]: never; }>;
+export type PokemonsQueryVariables = Exact<{ [key: string]: never }>
 
-
-export type PokemonsQuery = { __typename?: 'Query', pokemons?: Array<{ __typename?: 'Pokemon', id: string, number?: string | null, name?: string | null, classification?: string | null, types?: Array<string | null> | null, resistant?: Array<string | null> | null, weaknesses?: Array<string | null> | null, fleeRate?: number | null, maxCP?: number | null, maxHP?: number | null, image?: string | null, weight?: { __typename?: 'PokemonDimension', minimum?: string | null, maximum?: string | null } | null, height?: { __typename?: 'PokemonDimension', minimum?: string | null, maximum?: string | null } | null } | null> | null };
-
+export type PokemonsQuery = {
+  __typename?: "Query"
+  pokemons?: Array<{
+    __typename?: "Pokemon"
+    id: string
+    number?: string | null
+    name?: string | null
+    classification?: string | null
+    types?: Array<string | null> | null
+    resistant?: Array<string | null> | null
+    weaknesses?: Array<string | null> | null
+    fleeRate?: number | null
+    maxCP?: number | null
+    maxHP?: number | null
+    image?: string | null
+    weight?: {
+      __typename?: "PokemonDimension"
+      minimum?: string | null
+      maximum?: string | null
+    } | null
+    height?: {
+      __typename?: "PokemonDimension"
+      minimum?: string | null
+      maximum?: string | null
+    } | null
+  } | null> | null
+}
 
 export const PokemonsDocument = `
     query pokemons {
@@ -157,17 +190,19 @@ export const PokemonsDocument = `
     image
   }
 }
-    `;
-export const usePokemonsQuery = <
-      TData = PokemonsQuery,
-      TError = unknown
-    >(
-      dataSource: { endpoint: string, fetchParams?: RequestInit },
-      variables?: PokemonsQueryVariables,
-      options?: UseQueryOptions<PokemonsQuery, TError, TData>
-    ) =>
-    useQuery<PokemonsQuery, TError, TData>(
-      variables === undefined ? ['pokemons'] : ['pokemons', variables],
-      fetcher<PokemonsQuery, PokemonsQueryVariables>(dataSource.endpoint, dataSource.fetchParams || {}, PokemonsDocument, variables),
-      options
-    );
+    `
+export const usePokemonsQuery = <TData = PokemonsQuery, TError = unknown>(
+  dataSource: { endpoint: string; fetchParams?: RequestInit },
+  variables?: PokemonsQueryVariables,
+  options?: UseQueryOptions<PokemonsQuery, TError, TData>
+) =>
+  useQuery<PokemonsQuery, TError, TData>(
+    variables === undefined ? ["pokemons"] : ["pokemons", variables],
+    fetcher<PokemonsQuery, PokemonsQueryVariables>(
+      dataSource.endpoint,
+      dataSource.fetchParams || {},
+      PokemonsDocument,
+      variables
+    ),
+    options
+  )
