9e5f6849a8196faa23b15db218cbefd3c6c9005e
Dr. Derek Austin <derekraustin@gmail.com>
2022-05-09 22:56:50 -0500

    feat: Show Pokémon #1 (Bulbasaur) on the homepage route / using rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     // Configure next/image to use the website returned by the GraphQL API:
     domains: ["img.pokemondb.net"],
   },
+  async rewrites() {
+    return [
+      {
+        // Show Pokémon #1 (Bulbasaur) on the homepage route / using rewrites:
+        source: "/",
+        destination: "/1",
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
