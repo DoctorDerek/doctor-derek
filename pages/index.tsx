4ad443acdfa926ce5d3806f6620415a2cf0741b2
Dr. Derek Austin <derekraustin@gmail.com>
2022-08-12 21:53:07 -0500

    feat: create homepage using ColorHunt palette
    https://colorhunt.co/palette/1ffffffffde1ff9d76ff4273/?ref=tab

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className="grid h-screen w-screen grid-cols-1 grid-rows-4 text-center text-4xl font-bold">
+      <h1 className="flex w-full items-center justify-center rounded-xl bg-[#1FFFFF] p-1 text-6xl">
+        Dr. Derek Austin 🥳 @DoctorDerek
+      </h1>
+      <h1 className="flex w-full items-center justify-center rounded-xl bg-[#FFFDE1] p-1">
+        Developer Advocate at Propel Data ♦ Senior Frontend Developer
+      </h1>
+      <h1 className="flex w-full items-center justify-center rounded-xl bg-[#FF9D76] p-1">
+        React Software Engineer ♦ SEO & Web Performance Expert
+      </h1>
+      <h1 className="flex w-full items-center justify-center rounded-xl bg-[#FF4273] p-1">
+        My Favorite Tech Stack: Next + Tailwind
+      </h1>
+    </div>
+  )
+}
