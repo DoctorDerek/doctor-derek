dd9890a7612de270db6d51e9467dbce3a56703e5
Dr. Derek Austin <derekraustin@gmail.com>
2022-08-26 16:56:14 -0500

    fix: correct text on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,24 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <h1 className="flex w-full items-center justify-center rounded-xl bg-[#1FFFFF] p-1 text-6xl">
-        Dr. Derek Austin 🥳 @DoctorDerek
+        Dr. Derek Austin 🥳
       </h1>
       <h1 className="flex w-full items-center justify-center rounded-xl bg-[#FFFDE1] p-1">
-        Developer Advocate at Propel Data ♦ Senior Frontend Developer
+        I work exclusively with Next.js + Tailwind CSS
       </h1>
       <h1 className="flex w-full items-center justify-center rounded-xl bg-[#FF9D76] p-1">
-        React Software Engineer ♦ SEO & Web Performance Expert
+        Senior Frontend Developer ♦ React Software Engineer ♦ SEO & Web
+        Performance Expert ♦ I love accessible websites
       </h1>
       <h1 className="flex w-full items-center justify-center rounded-xl bg-[#FF4273] p-1">
-        My Favorite Tech Stack: Next + Tailwind
+        <a href="https://DoctorDerek.Medium.com">Medium.com/@DoctorDerek</a>{" "}
+        <a href="https://LinkedIn.com/in/derek-austin">
+          LinkedIn.com/in/Derek-Austin
+        </a>
+        <a href="https://github.com/doctorderek">GitHub.com/DoctorDerek</a>
+        <a href="https://twitter.com/AskDoctorDerek">
+          Twitter.com/AskDoctorDerek
+        </a>
       </h1>
     </div>
   )
