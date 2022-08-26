3b1bdb6a202f6b70d79431c5ef076fd750cd3470
Dr. Derek Austin <derekraustin@gmail.com>
2022-08-26 16:58:52 -0500

    fix: font sizes text-4xl to text-3xl, text-base

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head"
 
 export default function Home() {
   return (
-    <div className="grid h-screen w-screen grid-cols-1 grid-rows-4 text-center text-4xl font-bold">
+    <div className="grid h-screen w-screen grid-cols-1 grid-rows-4 text-center font-bold">
       <Head>
         <title>Dr. Derek Austin - Developer Advocate - @DoctorDerek</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -10,14 +10,14 @@ export default function Home() {
       <h1 className="flex w-full items-center justify-center rounded-xl bg-[#1FFFFF] p-1 text-6xl">
         Dr. Derek Austin 🥳
       </h1>
-      <h2 className="flex w-full items-center justify-center rounded-xl bg-[#FFFDE1] p-1">
+      <h2 className="flex w-full items-center justify-center rounded-xl bg-[#FFFDE1] p-1 text-5xl">
         I work exclusively with Next.js + Tailwind CSS
       </h2>
-      <h3 className="flex w-full items-center justify-center rounded-xl bg-[#FF9D76] p-1">
+      <h3 className="flex w-full items-center justify-center rounded-xl bg-[#FF9D76] p-1 text-3xl">
         Senior Frontend Developer ♦ React Software Engineer ♦ SEO & Web
         Performance Expert ♦ I love accessible websites
       </h3>
-      <h4 className="flex w-full items-center justify-center rounded-xl bg-[#FF4273] p-1">
+      <h4 className="flex w-full items-center justify-center rounded-xl bg-[#FF4273] p-1 text-base">
         <a href="https://DoctorDerek.Medium.com">Medium.com/@DoctorDerek</a>{" "}
         <a href="https://LinkedIn.com/in/derek-austin">
           LinkedIn.com/in/Derek-Austin
