de1dd77516484c9009665ea46234ae8a549b2d44
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-09-24 08:48:06 -0700

    feat: added footer to contactSection component

--- components/contactSection.tsx
+++ components/contactSection.tsx
@@ -1,28 +1,32 @@
 import Image from "next/image"
 import contactimage from "@/images/contactimage.png"
+import Footer from "./footer"
 
 const ContactSection = () => {
   return (
-    <div className="h-screen bg-[#311B4D]">
-      <div className="w-4/5 mx-auto">
-        <div className="pt-4 pb-4">
-          <h2 className="text-6xl text-[#FFE366]">Contact</h2>
-        </div>
-        {/*========= DR DEREK AUSTIN IMAGE ========= */}
-        <div className="">
-          <Image
-            src={contactimage}
-            alt="Contact Dr Derek Austin"
-            object-fit="contain"
-          />
+    <div className="bg-[#311B4D]">
+      <div className="flex h-screen flex-col">
+        <div className="mx-auto h-[60vh] w-4/5">
+          <div className="pb-4 pt-4">
+            <h2 className="text-6xl text-[#FFE366]">Contact</h2>
+          </div>
+          {/*========= DR DEREK AUSTIN IMAGE ========= */}
           <div className="">
-            <p className="pt-3 pr-4 leading-7 text-[#89CFFD] text-lg w-5/6 ">
-              Discover the power of versatile frontend developing with Derek
-              Austin. Get in touch to discuss your next development project, or
-              to simply chat.
-            </p>
+            <Image
+              src={contactimage}
+              alt="Contact Dr Derek Austin"
+              object-fit="contain"
+            />
+            <div className="">
+              <p className="w-5/6 pr-4 pt-3 text-lg leading-7 text-[#89CFFD] ">
+                Discover the power of versatile frontend developing with Derek
+                Austin. Get in touch to discuss your next development project,
+                or to simply chat.
+              </p>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
     </div>
   )
