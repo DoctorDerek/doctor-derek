52b637185744336aea4f793ddcdc509806739e62
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-10-06 17:56:08 -0700

    feat: added medium breakpoint styles for contact section

--- components/contactSection.tsx
+++ components/contactSection.tsx
@@ -4,31 +4,33 @@ import Footer from "./footer"
 
 const ContactSection = () => {
   return (
-    <div className="bg-[#311B4D]">
-      <div className="flex h-screen flex-col">
-        <div className="mx-auto w-4/5">
-          <div className="py-4">
-            <h2 className="text-7xl text-[#FFE366]">Contact</h2>
+    <div className="purple-bg-img bg-[#311B4D]">
+      <div className="flex h-screen flex-col md:flex-row">
+        <div className="mx-auto w-4/5 md:w-[45%] md:flex md:flex-col md:pl-8">
+          <div className="py-4 md:mt-auto md:mb-10">
+            <h2 className="text-7xl text-[#FFE366] md:text-8xl">Contact</h2>
           </div>
           {/*========= DR DEREK AUSTIN IMAGE ========= */}
-          <div className="">
+          <div className="md:mb-auto">
             <Image
               src={contactimage}
               alt="Contact Dr Derek Austin"
-              className="w-2/3"
+              className="w-2/3 md:w-full md:mb-32"
             />
           </div>
         </div>
 
-        <div className="w-4/5 mx-auto mt-10">
-          <p className="pr-4 text-lg leading-7 text-[#89CFFD]">
-            Discover the power of versatile frontend developing with Derek
-            Austin. Get in touch to discuss your next development project, or to
-            simply chat.
-          </p>
+        <div className="mx-auto mt-auto flex flex-col md:h-full md:w-[55%]">
+          <div className="mx-auto mb-12 w-4/5 md:h-1/6 md:w-4/5 md:mt-[45%] md:mb-0 ">
+            <p className="pr-4 text-lg leading-7 text-[#89CFFD]">
+              Discover the power of versatile frontend developing with Derek
+              Austin. Get in touch to discuss your next development project, or
+              to simply chat.
+            </p>
+          </div>
+          {/* ======Footer ======== */}
+          <Footer />
         </div>
-        {/* ======Footer ======== */}
-        <Footer />
       </div>
     </div>
   )
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,3 @@
-import IconLinks from "./iconLinks"
 import EmailIcon from "./emailIcon"
 import LinkedinIcon from "./linkedinIcon"
 import ResumeIcon from "./resumeIcon"
@@ -8,11 +7,11 @@ import BookLinkIcon from "./bookLinkIcon"
 
 export default function Footer() {
   return (
-    <footer className="mt-auto" aria-labelledby="footer-heading">
-      <div className="mx-auto max-w-7xl sm:pt-24 lg:px-8 lg:pt-32">
+    <footer className="mt-auto md:w-full" aria-labelledby="footer-heading">
+      <div className="mx-auto lg:px-8 lg:pt-32">
         <div className="bg-[#311B4D]">
           {/* ======= SIGN UP ========== */}
-          <div className="rounded-tl-2xl bg-[#FB70AA] py-6 xl:mt-0">
+          <div className="rounded-tl-[3rem] bg-[#FB70AA] py-6 md:w-4/5 md:ml-auto md:px-4">
             <div className="mx-auto w-11/12">
               <h3 className="pb-2 text-xl">Join over 160 email subscribers</h3>
               <form className="flex">
@@ -22,13 +21,13 @@ export default function Footer() {
                   id="email-address"
                   autoComplete="email"
                   required
-                  className="w-9/12 min-w-0 appearance-none border-0 bg-white px-3 py-1.5 text-base shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-lg placeholder:text-slate-400 focus:ring-2 focus:ring-inset sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                  className="w-9/12 appearance-none border-0 bg-white px-3 py-1.5 text-base shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-lg placeholder:text-slate-400 focus:ring-2 focus:ring-inset sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                   placeholder="Your email address"
                 />
-                <div className="w-1/4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+                <div className="w-1/4">
                   <button
                     type="submit"
-                    className="flex w-full items-center justify-center rounded-tr-md bg-[#311B4D] px-3 py-6 text-sm font-semibold text-[#FFE366] shadow-sm"
+                    className="flex w-full items-center justify-center rounded-tr-md bg-[#311B4D] px-3 py-6 text-md font-semibold text-[#FFE366] shadow-sm"
                   >
                     Sign up
                   </button>
@@ -37,8 +36,8 @@ export default function Footer() {
             </div>
           </div>
           {/* ======= FOOTER LINKS ========== */}
-          <div className="bg-[#FB70AA] sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
-            <div className="flex justify-around bg-[#FFE366] py-4">
+          <div className="bg-[#FB70AA] md:flex md:items-center md:justify-between lg:mt-24">
+            <div className="flex justify-around bg-[#FFE366] py-4 md:grid grid-rows-3 grid-cols-2 md:w-full md:gap-y-3  md:gap-x-1 md:jusitify-center">
               <EmailIcon fill="#F38B57" />
               <LinkedinIcon fill="#F38B57" />
               <ResumeIcon fill="#F38B57" />
