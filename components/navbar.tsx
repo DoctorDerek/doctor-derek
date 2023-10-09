7741ea8c556dcd05b2a2c4ae45a64a81a37cf79b
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-10-09 16:33:15 -0700

    refactor: refactored navbar to allow easier future changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,15 @@
-import { Fragment, useState } from "react"
+import { useState } from "react"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
 import Logo from "images/Logo.png"
+import medLrgLogo from "images/medLrgLogo.svg"
 import Link from "next/link"
-import IconLinks from "./iconLinks"
-import NavIcons from "./navIcons"
+import EmailIcon from "./emailIcon"
+import LinkedinIcon from "./linkedinIcon"
+import ResumeIcon from "./resumeIcon"
+import GithubIcon from "./githubIcon"
+import MediumIcon from "./mediumIcon"
+import BookLinkIcon from "./bookLinkIcon"
 
 const navigation = [
   { name: "About", href: "#", current: true },
@@ -19,23 +24,31 @@ export default function Navbar() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
-    <div className="h-[10vh]">
+    <div className="h-[7vh]">
       {/* ======== NAVBAR ======= */}
-      <div className="sticky top-0 z-40 flex h-full bg-[#FFE366] shadow-sm sm:gap-x-6 sm:px-6 md:h-screen md:px-0 md:w-14">
+      <div className="sticky top-0 z-40 flex h-full bg-[#FFE366] shadow-sm sm:gap-x-6 sm:px-6 md:h-screen md:w-14 md:px-0">
         <div className="flex w-full md:flex-col">
           <div className="w-1/4 pl-3 pt-3 md:order-2 md:h-[40vh] md:w-full md:pl-0 md:pt-0">
             <Link href="/" className="md:block md:h-1/2 md:w-full md:pt-4">
+              {/* Displays logo on small devices */}
               <Image
                 src={Logo}
                 object-fit="contain"
                 alt="Logo"
-                className="md:-rotate-90 md:py-4 "
+                className="md:hidden"
               />
+               {/* Displays logo on medium and large devices */}
+              <Image
+                src={medLrgLogo}
+                alt="Logo"
+                className="hidden md:block md:mx-auto"
+              />
+
             </Link>
           </div>
           <button
             type="button"
-            className="text-[#FFE366]md:mt-60 ml-auto bg-[#89cffd] p-3.5 md:order-1 md:ml-0"
+            className="text-[#FFE366]md:mt-60 ml-auto bg-[#89cffd] px-3.5 pt-1 md:order-1 md:ml-0 md:py-3"
             onClick={() => setSidebarOpen(!sidebarOpen)}
           >
             {sidebarOpen ? (
@@ -65,14 +78,14 @@ export default function Navbar() {
               sidebarOpen ? "translate-x-0" : "-translate-x-full"
             } `}
           >
-            <div className="flex h-full w-11/12 flex-col content-between rounded-tr-2xl bg-[#89cffd] md:w-3/4">
+            <div className="flex h-full w-11/12 flex-col content-between rounded-tr-2xl bg-[#89cffd] pl-5 md:w-3/4">
               <ul role="list" className="pt-8">
                 {navigation.map((item) => (
-                  <li key={item.name}>
+                  <li className="hover:text-white" key={item.name}>
                     <a
                       href={item.href}
                       className={
-                        "md:restora-bold block p-2 text-5xl font-semibold md:p-1 md:pr-12 md:text-end md:text-7xl"
+                        "md:restora-bold block py-2 text-5xl font-semibold md:p-1 md:pr-12 md:text-end md:text-7xl lg:text-8xl"
                       }
                     >
                       {item.name}
@@ -80,14 +93,64 @@ export default function Navbar() {
                   </li>
                 ))}
               </ul>
-              <div className="mt-auto w-10/12 pb-2 md:hidden">
-                <IconLinks fill={"white"} flexSpacing={"justify-around"} />
+              {/* Sidebar, displays icon links on small devices */}
+              <div className="mt-auto w-10/12 pb-6 md:hidden">
+                <div className="flex justify-between">
+                  <a href="mailto:derekraustin@gmail.com">
+                    <EmailIcon fill="white" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/derek-austin/"
+                    target="_blank"
+                  >
+                    <LinkedinIcon fill="white" />
+                  </a>
+                  <a href="">
+                    <ResumeIcon fill="white" />
+                  </a>
+                  <a href="https://github.com/DoctorDerek" target="_blank">
+                    <GithubIcon fill="white" />
+                  </a>
+                  <a href="https://doctorderek.medium.com/" target="_blank">
+                    <MediumIcon fill="white" />
+                  </a>
+                  <a
+                    href="https://www.amazon.com/dp/B0BRJDLJ43"
+                    target="_blank"
+                  >
+                    <BookLinkIcon fill="white" />
+                  </a>
+                </div>
               </div>
             </div>
-          <NavIcons fill={"#f38B57"} flexSpacing={"justify-center"} />
+             {/* Displays icons in sidebar on medium and large devices */}
+            <div className="mx-auto my-auto hidden flex-col justify-between gap-y-4 md:flex">
+              <a className="block mb-2" href="mailto:derekraustin@gmail.com">
+                <EmailIcon fill="#F38B57" />
+              </a>
+              <a
+              className="block mb-2"
+                href="https://www.linkedin.com/in/derek-austin/"
+                target="_blank"
+              >
+                <LinkedinIcon fill="#F38B57" />
+              </a>
+              <a className="block mb-2" href="">
+                <ResumeIcon fill="#F38B57" />
+              </a>
+              <a className="block mb-2" href="https://github.com/DoctorDerek" target="_blank">
+                <GithubIcon fill="#F38B57" />
+              </a>
+              <a className="block mb-2" href="https://doctorderek.medium.com/" target="_blank">
+                <MediumIcon fill="#F38B57" />
+              </a>
+              <a className="block mb-2" href="https://www.amazon.com/dp/B0BRJDLJ43" target="_blank">
+                <BookLinkIcon fill="#F38B57" />
+              </a>
+            </div>
           </nav>
         </div>
-      </div>     
+      </div>
     </div>
   )
 }
--- components/topSection.tsx
+++ components/topSection.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import M0_Intro_Animation from "@/images/M0_Intro_Animation.jpg"
-import D0_Intro_Animation from "@/images/D0_Intro_Animation.jpg"
 import Navbar from "./navbar"
 
 /** Helper function to join Tailwind CSS classNames. Filters out falsy values */
@@ -13,18 +12,18 @@ export default function TopSection({
   aspect: "aspect-[5760/3200]" | "aspect-[1500/2668]"
 }) {
   return (
-    <div className="md:h-[100vh] intro">
+    <div className="intro md:h-[100vh]">
       <Navbar />
-        <Image
-          src={M0_Intro_Animation}
-          alt={"Intro_Animation"}
-          className={classNames(
-            // We use "bg-yellow" to fill in the background (sides)
-            "h-[90dvh] w-full object-contain md:hidden",
-            aspect,
-          )}
-          placeholder="blur"
-        />
+      <Image
+        src={M0_Intro_Animation}
+        alt={"Intro_Animation"}
+        className={classNames(
+          // We use "bg-yellow" to fill in the background (sides)
+          "h-[93dvh] w-full object-contain md:hidden",
+          aspect,
+        )}
+        placeholder="blur"
+      />
     </div>
   )
 }
