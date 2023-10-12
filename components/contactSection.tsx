b2150384579ef5bfedf67d62e89f2dc78ff87e6f
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-10-11 19:04:42 -0700

    refactor: refactored small and medium breakpoints for footer and contact section

--- components/contactSection.tsx
+++ components/contactSection.tsx
@@ -1,28 +1,47 @@
 import Image from "next/image"
 import contactimage from "@/images/contactimage.png"
+import DerekSpriteImg from "@/images/DerekSpriteImg.png"
 import Footer from "./footer"
 
 const ContactSection = () => {
   return (
     <div className="purple-bg-img bg-[#311B4D]">
-      <div className="flex h-screen flex-col md:flex-row">
-        <div className="mx-auto w-4/5 h-1/2 md:w-1/2 md:flex md:flex-col md:pl-8 md:h-full md:justify-center lg:justify-start lg:w-[40%]">
-          <div className="py-4 md:mb-10 lg:py-0 lg:mb-0">
-            <h2 className="text-7xl text-[#FFE366] md:text-8xl lg:text-9xl lg:pt-8 lg:pb-14">Contact</h2>
+      <div className=" h-screen flex flex-col md:flex-row">
+        <div className="mx-auto w-[85%] h-[45%] md:flex md:flex-col md:h-full md:w-1/2 md:pl-8 lg:w-[40%] lg:justify-start">
+          <div className="py-4 md:mb-2 md:mt-24 lg:mb-0 lg:py-0">
+            <h2 className="text-7xl text-[#FFE366] md:text-8xl lg:pb-14 lg:pt-8 lg:text-9xl">
+              Contact
+            </h2>
           </div>
-          {/*========= DR DEREK AUSTIN IMAGE ========= */}
-          <div className="">
-            <Image
-              src={contactimage}
-              alt="Contact Dr Derek Austin"
-              className="w-3/4 md:w-11/12"
-            />
+
+          {/* Flipping images */}
+          <div className="w-3/5 flip md:w-full md:h-1/2 ">
+            <div className="">
+              <div className="wrapper md:relative hover:cursor-pointer">
+                {/*==== Front image ======  md:h-3/4 md:w-[85%]*/}
+                <div className="front md:h-3/5">
+                  <Image
+                    src={contactimage}
+                    alt="John Doe"
+                    className="md:z-50 md:relative"
+                  />
+                </div>
+                 {/*===== Back image ======= */}
+                <div className="hidden md:block md:py-6 back md:absolute md:top-0 md:right-0 md:bottom-0 md:left-0">
+                  <Image
+                    src={DerekSpriteImg}
+                    alt="John Doe"
+                    className="md:z-50 md:relative "
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="mx-auto mt-auto flex flex-col h-1/2 w-full md:h-full md:w-1/2 md:pl-4 md:mt-0 lg:ml-auto lg:w-[60%]">
-          <div className="mx-auto mb-12 w-4/5 md:h-1/6 md:w-4/5 md:mb-auto md:mt-auto md:mx-0 md:pt-16 lg:my-auto lg:pt-12 lg:pl-14">
-            <p className="pl-4 text-lg leading-8 text-[#89CFFD] lg:text-2xl md:pl-0">
+        <div className="mx-auto h-[55%] w-full flex flex-col md:mt-0 md:h-full md:w-1/2 md:pl-8 lg:ml-auto">
+          <div className="mx-auto mb-12 w-4/5 md:mx-auto md:mt-40 md:h-1/6 md:w-11/12  lg:my-auto lg:pl-14 lg:pt-12">
+            <p className="pl-2 text-xl leading-8 text-[#89CFFD] md:text-xl md:pl-0 lg:text-2xl">
               Discover the power of versatile frontend developing with Derek
               Austin. Get in touch to discuss your next development project, or
               to simply chat.
--- components/footer.tsx
+++ components/footer.tsx
@@ -8,12 +8,14 @@ import BookLinkIcon from "./bookLinkIcon"
 export default function Footer() {
   return (
     <footer className="mt-auto md:w-full" aria-labelledby="footer-heading">
-      <div className="mx-auto lg:pt-32 lg:w-full lg:ml-auto lg:mr-0 lg:pl-12">
+      <div className="mx-auto lg:ml-auto lg:mr-0 lg:w-full lg:pl-12 lg:pt-32">
         <div className="">
           {/* ======= SIGN UP ========== */}
-          <div className="rounded-tl-3xl md:rounded-tl-[3.5rem] bg-[#FB70AA] py-6 md:py-8 md:pl-4 md:ml-auto md:w-4/5 lg:pb-16 lg:pt-20 lg:pl-16">
+          <div className="rounded-tl-3xl bg-[#FB70AA] py-6 md:ml-auto md:w-4/5 md:rounded-tl-[3.5rem] md:py-8 md:pl-4 lg:pb-16 lg:pl-16 lg:pt-20">
             <div className="mx-auto w-11/12">
-              <h3 className="pb-3 text-xl md:pb-4 lg:text-2xl">Join over 160 email subscribers</h3>
+              <h3 className="pb-3 text-xl md:text-lg md:pb-4 lg:text-2xl">
+                Join over 160 email subscribers
+              </h3>
               <form className="flex">
                 <input
                   type="email"
@@ -21,13 +23,13 @@ export default function Footer() {
                   id="email-address"
                   autoComplete="email"
                   required
-                  className="w-9/12 appearance-none border-0 bg-white px-3 py-1.5 text-base shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-lg placeholder:text-slate-400 focus:ring-2 focus:ring-inset sm:w-64 sm:text-sm sm:leading-6 lg:py-5 lg:pl-4 lg:w-3/5"
+                  className="w-9/12 appearance-none border-0 bg-white px-3 py-1.5 text-base shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-lg placeholder:text-slate-400 focus:ring-2 focus:ring-inset sm:w-64 sm:text-sm sm:leading-6 lg:w-3/5 lg:py-5 lg:pl-4"
                   placeholder="Your email address"
                 />
-                <div className="w-1/4 md:w-1/3 lg:w-1/4">
+                <div className="w-1/4 md:w-[40%] lg:w-1/4">
                   <button
                     type="submit"
-                    className="text-md flex w-full items-center justify-center rounded-tr-2xl bg-[#311B4D] px-3 py-6 shadow-sm md:py-3 lg:py-5 font-semibold text-[#FFE366] md:text-lg lg:text-xl"
+                    className="text-md w-full rounded-tr-2xl bg-[#311B4D] px-3 py-6 font-semibold text-[#FFE366] shadow-sm md:py-4 lg:py-5 lg:text-xl"
                   >
                     Sign up
                   </button>
@@ -37,14 +39,50 @@ export default function Footer() {
           </div>
           {/* ======= FOOTER LINKS ========== */}
           <div className="bg-[#FB70AA] md:bg-transparent">
-            <div className="bg-[#FFE366] rounded-tl-3xl md:rounded-tl-[3.5rem]">
-              <div className="flex justify-around grid-cols-2 grid-rows-3 py-6 md:pt-8 md:grid md:gap-y-6 md:w-3/5 md:pl-8 lg:pl-14 lg:pb-16 lg:pt-14">
-                <EmailIcon fill="#F38B57" />
-                <LinkedinIcon fill="#F38B57" />
-                <ResumeIcon fill="#F38B57" />
-                <GithubIcon fill="#F38B57" />
-                <MediumIcon fill="#F38B57" />
-                <BookLinkIcon fill="#F38B57" />
+            <div className="rounded-tl-3xl bg-[#FFE366] md:rounded-tl-[3.5rem]">
+              <div className="flex justify-around grid-cols-2 grid-rows-3 py-6 md:grid md:gap-y-2.5 md:ml-8 md:py-8 md:w-3/4 lg:pb-16 lg:pl-14 lg:pt-14">
+                <a className="text-lg text-[#311B4D] md:flex"
+                  href="mailto:derekraustin@gmail.com"
+                >
+                  <EmailIcon fill="#F38B57" />
+                  <span className="ml-2 hidden pt-1 md:block restoramedium">Email</span>
+                </a>
+                <a
+                  className="text-lg text-[#311B4D] md:flex"
+                  href="https://www.linkedin.com/in/derek-austin/"
+                  target="_blank"
+                >
+                  <LinkedinIcon fill="#F38B57" />
+                  <span className="ml-2 hidden pt-1 md:block restoramedium">Linkedin</span>
+                </a>
+                <a href="#" className="text-lg text-[#311B4D] md:flex">
+                  <ResumeIcon fill="#F38B57" />
+                  <span className="ml-2 hidden pt-1 md:block restoramedium">Resume</span>
+                </a>
+                <a
+                  href="https://github.com/DoctorDerek"
+                  className="text-lg text-[#311B4D] md:flex"
+                  target="_blank"
+                >
+                  <GithubIcon fill="#F38B57" />
+                  <span className="ml-2 hidden pt-1 md:block restoramedium">Github</span>
+                </a>
+                <a
+                  href="https://doctorderek.medium.com/"
+                  className="text-lg text-[#311B4D] md:flex"
+                  target="_blank"
+                >
+                  <MediumIcon fill="#F38B57" />
+                  <span className="ml-2 hidden pt-1 md:block restoramedium">Medium</span>
+                </a>
+                <a
+                  className="text-lg text-[#311B4D] md:flex"
+                  href="https://www.amazon.com/dp/B0BRJDLJ43"
+                  target="_blank"
+                >
+                  <BookLinkIcon fill="#F38B57" />
+                  <span className="ml-2 hidden pt-1 md:block restoramedium">Book</span>
+                </a>
               </div>
             </div>
           </div>
