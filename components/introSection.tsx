3e102a0f3dd012af034cf2d0d31021cc74e37775
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-10-12 09:10:15 -0700

    refactor: removed iconLinks component for better maintainability

--- components/introSection.tsx
+++ components/introSection.tsx
@@ -1,31 +1,92 @@
-import IconLinks from "./iconLinks"
+import EmailIcon from "./emailIcon"
+import LinkedinIcon from "./linkedinIcon"
+import ResumeIcon from "./resumeIcon"
+import GithubIcon from "./githubIcon"
+import MediumIcon from "./mediumIcon"
+import BookLinkIcon from "./bookLinkIcon"
 
 const IntroSection = () => {
   return (
-    <div className="flex flex-col h-[90vh] yw-bg-img md:h-screen">
-      <div className="w-4/5 md:w-[90%] mx-auto pt-4">
-        <p className="text-[#FB70AA] text-3xl md:text-5xl lg:text-7xl">
+    <div className="yw-bg-img flex h-[90vh] flex-col md:h-screen">
+      <div className="mx-auto w-4/5 pt-4 md:w-[90%]">
+        <p className="text-3xl text-[#FB70AA] md:text-5xl lg:text-7xl">
           React Software Engineer specializing in optimizing web performance,
           enhancing accessiblility, and crafting highly readable code.
         </p>
       </div>
       {/* ========= ICON LINKS ============ */}
-      <div className="w-4/5 md:w-[90%] mx-auto pt-4 mt-auto lg:mb-8">
-        <div className="w-3/4 lg:w-[60%]">
-          <IconLinks fill={"#f38B57"} flexSpacing={"justify-between"} />
-          <div className="border-t-2 border-[#d6bb61] mt-4 md:w-5/12 lg:mt-8 lg:w-1/3"/>
+      <div className="mx-auto mt-auto w-4/5 pt-4 md:w-[90%] lg:mb-8">
+        <div className="w-3/4 md:w-11/12 lg:w-3/5">
+          <div className="flex justify-around py-6 md:mx-auto lg:justify-between">
+            <a
+              className="text-lg text-[#F38B57] md:flex md:mr-4 lg:text-xl"
+              href="mailto:derekraustin@gmail.com"
+            >
+              <EmailIcon fill="#F38B57" />
+              <span className="restoramedium ml-2 hidden pt-1 md:block">
+                Email
+              </span>
+            </a>
+            <a
+              className="text-lg text-[#F38B57] md:flex md:mr-4 lg:text-xl"
+              href="https://www.linkedin.com/in/derek-austin/"
+              target="_blank"
+            >
+              <LinkedinIcon fill="#F38B57" />
+              <span className="restoramedium ml-2 hidden pt-1 md:block">
+                Linkedin
+              </span>
+            </a>
+            <a href="#" className="text-lg  text-[#F38B57] md:flex md:mr-4 lg:text-xl">
+              <ResumeIcon fill="#F38B57" />
+              <span className="restoramedium ml-2 hidden pt-1 md:block">
+                Resume
+              </span>
+            </a>
+            <a
+              href="https://github.com/DoctorDerek"
+              className="text-lg text-[#F38B57] md:flex md:mr-4 lg:text-xl"
+              target="_blank"
+            >
+              <GithubIcon fill="#F38B57" />
+              <span className="restoramedium ml-2 hidden pt-1 md:block">
+                Github
+              </span>
+            </a>
+            <a
+              href="https://doctorderek.medium.com/"
+              className="text-lg text-[#F38B57] md:flex md:mr-4 lg:text-xl"
+              target="_blank"
+            >
+              <MediumIcon fill="#F38B57" />
+              <span className="restoramedium ml-2 hidden pt-1 md:block">
+                Medium
+              </span>
+            </a>
+            <a
+              className="lg:xl text-lg text-[#F38B57] md:flex"
+              href="https://www.amazon.com/dp/B0BRJDLJ43"
+              target="_blank"
+            >
+              <BookLinkIcon fill="#F38B57" />
+              <span className="restoramedium ml-2 hidden pt-1 md:block">
+                Book
+              </span>
+            </a>
+          </div>
+          <div className="mt-4 border-t-2 border-[#d6bb61] md:w-5/12 lg:mt-8 lg:w-1/3" />
         </div>
       </div>
 
       {/* ========= CLIENTS & LOGOs ============ */}
-      <div className="w-4/5 mx-auto  md:w-[90%] mt-2 mb-10">
-        <p className="text-[#998F8F] text-2xl font-extralight pt-3 pb-3 lg:mb-8 lg:text-3xl">
+      <div className="mx-auto mb-10  mt-2 w-4/5 md:w-[90%]">
+        <p className="pb-3 pt-3 text-2xl font-extralight text-[#998F8F] lg:mb-8 lg:text-3xl">
           Clients
         </p>
         <div className="flex w-full justify-between">
-          <p className="w-1/4 text-[#998F8F] text-4xl font-medium">Logo</p>
-          <p className="w-1/4 text-[#998F8F] text-4xl font-medium">Logo</p>
-          <p className="w-1/4 text-[#998F8F] text-4xl font-medium">Logo</p>
+          <p className="w-1/4 text-4xl font-medium text-[#998F8F]">Logo</p>
+          <p className="w-1/4 text-4xl font-medium text-[#998F8F]">Logo</p>
+          <p className="w-1/4 text-4xl font-medium text-[#998F8F]">Logo</p>
         </div>
       </div>
     </div>
